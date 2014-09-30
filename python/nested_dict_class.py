1c0530ad2f171a03431b4401758c647fdc9b3e30
Qingkai Kong <kongqk@berkeley.edu>
2014-09-29 21:26:22 -0700

    Add class that allow python to have nested dictionary

---
+++ python/nested_dict_class.py
@@ -0,0 +1,10 @@
+##class to allow nested dictionary
+
+class nested_dict(dict):
+    """Implementation of perl's autovivification feature."""
+    def __getitem__(self, item):
+        try:
+            return dict.__getitem__(self, item)
+        except KeyError:
+            value = self[item] = type(self)()
+            return value
