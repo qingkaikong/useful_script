20243dada2a43b6a58037484aaa5e55b971716db
Qingkai Kong <kongqk@berkeley.edu>
2014-10-04 18:33:18 -0700

    add a comment to show how to read a field of string

--- python/read_formated_txt_file/read_formated_txt.py
+++ python/read_formated_txt_file/read_formated_txt.py
@@ -4,6 +4,7 @@ import numpy as np
 
 convertfunc = lambda x: float(x.strip("%"))/100
 
+#if you want to read in strings, it should be (str, 9)
 dt = dict(names = ('Week', 'ice_cream', 'ice_cream_std', 'full_moon', 'full_moon_std'), formats = (np.float64, np.float64, np.float32, np.float64, np.float32))
 
 data = np.loadtxt('astro.csv', delimiter=',', dtype=dt, converters={0: md.datestr2num, 2: convertfunc, 4: convertfunc}, skiprows=1)
