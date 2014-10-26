d3bb663738d15bcd911416625c0511b9bf0cdc23
Qingkai Kong <kongqk@berkeley.edu>
2014-10-25 23:23:12 -0700

    Adding a moving average script

---
+++ python/moving_average.py
@@ -0,0 +1,7 @@
+def movingaverage(values,window):
+    weigths = np.repeat(1.0, window)/window
+    #including valid will REQUIRE there to be enough datapoints.
+    #for example, if you take out valid, it will start @ point one,
+    #not having any prior points, so itll be 1+0+0 = 1 /3 = .3333
+    smas = np.convolve(values, weigths, 'valid')
+    return smas # as a numpy array
