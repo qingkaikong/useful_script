7b3bae71da1b7b220e191b8f37e18d940c025677
Qingkai Kong <qingkai.kong@gmail.com>
2016-08-04 11:52:52 -0700

    add example 10

--- gmt/examples.py
+++ gmt/examples.py
@@ -56,3 +56,13 @@ gmt psxy data -R -J -O -W -Si0.2i >> GMT_tut_8.ps
 # 
 gmt pscoast -R130/150/35/50 -JM6i -B5 -P -Ggray -K > GMT_tut_9.ps
 gmt psxy -R -J -O quakes.ngdc -Wfaint -i4,3,5,6s0.1 -h3 -Scc -Cquakes.cpt >> GMT_tut_9.ps
+
+####################################### Example 10
+# write text strings
+gmt pstext -R0/7/0/5 -Jx1i -P -Ba -F+f30p,Times-Roman,DarkOrange+jBL << EOF > GMT_tut_10.ps
+1  1  It's P@al, not Pal!
+1  2  Try @%33%ZapfChancery@%% today
+1  3  @~D@~g@-b@- = 2@~pr@~G@~D@~h.
+1  4  University of Hawaii at M@!a\225noa
+EOF
+
